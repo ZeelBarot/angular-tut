d7fcf9c3ce1352a257dcaf2dc89043314efdeedd
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-28 15:46:09 +0530

    Event Binding

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -13,4 +13,17 @@ export class DataBinding {
   className = "primary";
 
   inputType = "date"; // Dynamic Attribute Binding
+
+  // function name(){      // JS Function
+  //   return "Zeel Barot";
+  // }
+
+  showWelcomeMsg(){
+    alert("Welcome to Angular v-21 Full Course");
+  }
+
+  onCityChange(){
+    alert("City has been changed");
+  }
+
 }
