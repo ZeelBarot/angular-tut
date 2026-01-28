2f3a7eafc5f6ab72bc6bf839de379c59a83cb6c4
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-28 13:11:24 +0530

    Essential Variables and datatype

--- src/app/components/variables/variables.ts
+++ src/app/components/variables/variables.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './variables.css',
 })
 export class Variables {
+  // var courseName = "Angular 21 Tutorial";  // JS string variable
+  // string || number || boolean || Date || array
+
+  courseName: string = "Angular 21 Tutorial"; // TS string variable
+  currentVersion = "v.21"
+
+  rollNo: number = 45;  // TS number variable
+  percentage: number = 88.5;
+
+  isActive: boolean = true;  // TS boolean variable
+  isPresent: boolean = false;
+
+  currentDate: Date = new Date(); // TS Date variable
+
+  topics: string[] = ['Components', 'Modules', 'Templates', 'Data Binding']; // TS array variable
+  cityList: string[] = ['Pune', 'Mumbai', 'Delhi', 'Bangalore'];
+
+  studentObj = {
+    name: 'Zeel Barot',
+    mobile: 1234567890,
+    email: 'zeel.barot@example.com'
+  }
+
+  studentList = [
+    { name: 'Alice', city: 'Pune' },
+    { name: 'Bob', city: 'Mumbai' },
+    { name: 'Charlie', city: 'Delhi' }
+  ]
+
+  constructor() {
+    // this.courseName = 21; // Error: Type 'number' is not assignable to type 'string'
+    this.rollNo = 50;
+  }
 
 }
