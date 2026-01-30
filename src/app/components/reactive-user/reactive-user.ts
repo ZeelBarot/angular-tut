3e52264820f5c407a1074e07713c1524174c7c42
Darji Nirali J <161916029+ZeelBarot@users.noreply.github.com>
2026-01-30 18:14:56 +0530

    Reactive Form Validation

--- src/app/components/reactive-user/reactive-user.ts
+++ src/app/components/reactive-user/reactive-user.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-reactive-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-user.html',
   styleUrl: './reactive-user.css',
 })
 export class ReactiveUser {
-
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required,Validators.minLength(5)]),
+    username: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
+  });
 }
