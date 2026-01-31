2714111599099e6febb7c49c944a4d378116d705
Darji Nirali J <161916029+ZeelBarot@users.noreply.github.com>
2026-01-31 18:21:33 +0530

    service example

--- src/app/service/master.ts
+++ src/app/service/master.ts
@@ -4,5 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Master {
-  
+  // store data
+  // create api call function 70%
+  // create subject or behaviotrSubject
+  // utility function & helper function
+
+  addTwoNum(num1: number, num2: number){
+    const sum = num1 + num2;
+    return sum;
+  }
 }
